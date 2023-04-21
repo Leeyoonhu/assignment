0eef6a3453d36dc702771fa2a51fb5eee918a1c3
Leeyoonhu <316902@naver.com>
2023-04-22 08:23:28 +0900

    상세정보 페이지 추가, 예약 과정 추가

--- src/main/java/com/assignment/controller/HomeController.java
+++ src/main/java/com/assignment/controller/HomeController.java
@@ -4,10 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
@@ -30,6 +33,13 @@ public class HomeController {
     @GetMapping("/login")
     public ModelAndView getLogin(){
         mav.setViewName("login");
+        return mav;
+    }
+
+    @GetMapping("/reserve/{hospName}")
+    public ModelAndView getReserve(@PathVariable String hospName){
+        mav.setViewName("reserve");
+        mav.addObject("hospName", hospName);
         return mav;
     }
 }
